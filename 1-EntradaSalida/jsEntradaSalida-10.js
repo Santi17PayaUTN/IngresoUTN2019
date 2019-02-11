612e089a7651525a2ddc0130b23a03578436d63c
Santi17PayaUTN <47321459+Santi17PayaUTN@users.noreply.github.com>
2019-02-11 12:34:19 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,8 +4,9 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function MostrarDescuento()
 {var importes; var descuentos; var porcentaje;
-    importes= parseInt(document.getElementById("importe").value);
-    porcentaje= ((importes*20)/100);
+    importes= parseFloat(document.getElementById("importe").value);
+    porcentaje= ((importes*25)/100);
     descuentos= (importes - porcentaje);
     document.getElementById("resultado").value = descuentos;
+    //Se utiliza parseFloat porque es dinero, en el cual el valor puede tener comas.
 }
